3280e234f0a44db7ffe7db10a3f1fb827deff35c
Eddie Bracho <eddieb@liatrio.com>
2016-12-26 21:25:14 -0800

    Implemented feature toggles

---
+++ feature-toggles.js
@@ -0,0 +1,13 @@
+// Feature toggle config. Used to enable/disable individual features.
+
+module.exports = {
+  'about': true,
+  'beerjar': true,
+  'echo': true,
+  'greet': true,
+  'nickname': true,
+  'promote': true,
+  'saveit': true,
+  'shutdown': true,
+  'whoami': true,
+};
