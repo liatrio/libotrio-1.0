3cd0800e0f82b5db0a8b07a3d9f5bea2cc1a98b6
Eddie Bracho <eddieb@liatrio.com>
2017-01-15 20:47:05 -0800

    Added Jira API Queries

--- feature-toggles.js
+++ feature-toggles.js
@@ -6,7 +6,7 @@ module.exports = {
   'echo': true,
   'greet': true,
   'help': true,
-  'jira': true,
+  'jira': !!process.env.JIRA_USER,
   'nickname': true,
   'promote': !!process.env.JENKINS_API_KEY,
   'saveit': true,
