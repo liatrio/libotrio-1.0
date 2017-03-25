9390688585b4db82d353ab3dd0b28c47d0ba1486
ebracho <eddieb@liatrio.com>
2017-03-25 14:01:31 -0700

    Updated Jenkins authentication logic

--- features/promote.js
+++ features/promote.js
@@ -4,24 +4,22 @@ var os = require('os');
 var config = require('../package');
 var request = require('request');
 
-function promote(bot, controller) {
+const jenkinsUser = process.env.JENKINS_USER
+const jenkinsApiToken = process.env.JENKINS_API_TOKEN
+const deployTriggerUrl = process.env.LIBOTRIO_DEPLOY_TRIGGER_URL
+const authedDeployTriggerUrl = `https://${jenkinsUser}:${jenkinsApiToken}@${deployTriggerUrl}`
 
-  if (!process.env.JENKINS_API_KEY) {
-    console.error('JENKINS_API_KEY environment variable requires for promote features.');
+function promote(bot, controller) {
+  if (!(jenkinsUser && jenkinsApiToken && deployTriggerUrl)) {
+    console.error('Promote feature requires JENKINS_USER, JENKINS_API_TOKEN, LIBOTRIO_DEPLOY_TRIGGER_URL environment variables.');
     return;
   }
 
-  var jobTriggerUrl = 'https://admin:' + process.env.JENKINS_API_KEY + '@build.liatrio.com/job/Libotrio/job/libotrio-deploy-production/build?token=libotrio-production-deploy';
-  console.log(jobTriggerUrl);
-
-  controller.hears(['promote to prod'],
-  'direct_message,direct_mention,mention', function(bot, message) {
-
+  controller.hears(['promote to prod'], 'direct_message,direct_mention,mention', function(bot, message) {
     request.get({
       'rejectUnauthorized': false,
-      'url': jobTriggerUrl,
+      'url': authedDeployTriggerUrl,
     }, function (error, response, body) {
-      console.log(error);
       if (!error && (response.statusCode == 200 || response.statusCode == 201)) {
         bot.reply(message, 'Deployment job triggered');
       }
@@ -29,7 +27,6 @@ function promote(bot, controller) {
         bot.reply(message,':middle_finger: ' + response.statusCode + ' ' + error);
       }
     });
-
   });
 }
 
