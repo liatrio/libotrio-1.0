0af0070f7bb39a657712645ce4206ad1e0ec6e72
Eddie Bracho <eddiebracho@gmail.com>
2017-07-19 16:21:48 -0700

    Added simple storage backend (#26)

--- tests/about.js
+++ tests/about.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const botMock = require('./mocks/botMock');
-const SimpleStorage = require('./storage/SimpleStorage');
+const SimpleStorage = require('../lib/storage/simple_storage');
 const aboutFeature = require('../features/about').feature;
 
 describe('about tests', () => {
