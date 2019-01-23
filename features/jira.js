f30efd8d63195b565bbc1b5bc050879d295b7d1d
Steven O <steveno@liatrio.com>
2019-01-23 14:24:52 -0800

    testing convo

--- features/jira.js
+++ features/jira.js
@@ -51,6 +51,57 @@ function jira(bot, controller) {
 
     });
 
+    controller.hears(['convo'], ['direct_message'], function(bot, message){
+        bot.startConversation(message, function(err, convo) {
+
+            convo.ask({
+                attachments:[
+                    {
+                        title: 'Do you want to proceed?',
+                        callback_id: '123',
+                        attachment_type: 'default',
+                        actions: [
+                            {
+                                "name":"yes",
+                                "text": "Yes",
+                                "value": "yes",
+                                "type": "button",
+                            },
+                            {
+                                "name":"no",
+                                "text": "No",
+                                "value": "no",
+                                "type": "button",
+                            }
+                        ]
+                    }
+                ]
+            },[
+                {
+                    pattern: "yes",
+                    callback: function(reply, convo) {
+                        convo.say('FABULOUS!');
+                        convo.next();
+                        // do something awesome here.
+                    }
+                },
+                {
+                    pattern: "no",
+                    callback: function(reply, convo) {
+                        convo.say('Too bad');
+                        convo.next();
+                    }
+                },
+                {
+                    default: true,
+                    callback: function(reply, convo) {
+                        // do nothing
+                    }
+                }
+            ]);
+        });
+    });
+
     // receive an interactive message, and reply with a message that will replace the original
     controller.on('interactive_message_callback', function (bot, message) {
         if (message.callback_id === 'board_select') {
