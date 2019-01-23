627644a2f820f333fb6035b1ac6153c250cdbf60
JGuzman4 <jeffg@liatrio.com>
2019-01-23 14:54:06 -0800

    reverting to convo

--- features/takeTicket.js
+++ features/takeTicket.js
@@ -1,135 +1,105 @@
 function takeTicket(bot, controller) {
 
   controller.hears(['assign-me (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
-    bot.startConversation(message, function(err, convo) {
+    if (!message.match[1]) {
+      bot.reply(message, "Please specify a ticket.");
+    }
+    else {
+      const request = require('request');
+      var JiraClient = require('jira-connector');
 
-    convo.ask({
-        attachments:[
-            {
-                title: 'Do you want to proceed?',
-                callback_id: '123',
-                attachment_type: 'default',
-                actions: [
-                    {
-                        "name":"yes",
-                        "text": "Yes",
-                        "value": "yes",
-                        "type": "button",
-                    },
-                    {
-                        "name":"no",
-                        "text": "No",
-                        "value": "no",
-                        "type": "button",
-                    }
-                ]
-            }
-        ]
-    },[
-        {
-            pattern: "yes",
-            callback: function(reply, convo) {
-                convo.say('FABULOUS!');
-                convo.next();
-                // do something awesome here.
-            }
-        },
-        {
-            pattern: "no",
-            callback: function(reply, convo) {
-                convo.say('Too bad');
-                convo.next();
-            }
-        },
-        {
-            default: true,
-            callback: function(reply, convo) {
-                // do nothing
-            }
+      var JIRA_CREDS = process.env.JIRA_API_CREDENTIALS;
+      var jira = new JiraClient( {
+        host: process.env.JIRA_HOST,
+        protocol: "http",
+        basic_auth: {
+          username: JIRA_CREDS.split(":")[0],
+          password: JIRA_CREDS.split(":")[1]
         }
-    ]);
-});
-    //const request = require('request');
-    //var JiraClient = require('jira-connector');
+      });
+      const auth = "Bearer " + process.env.SLACK_ACCESS_TOKEN;
+      const key = message.match[1];
+      const options = { 
+        method: 'GET', url: 'https://liatrio.slack.com/api/users.list',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': auth }
+      };
+      request(options, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          const list = JSON.parse(body);
+          for (var i = 0; i < list.members.length; i++){
+            if (list.members[i].id == message.user){
+              jira.issue.assignIssue({ issueKey: key, assignee: list.members[i].profile.email.split("@")[0]}, function(error, issue) {
+                if (error) { bot.reply(message, 'Oops! Something went wrong.'); console.log(error); }
+                else {
+                  jira.issue.getIssue({ issueKey: key}, function(error, is) {
+                    if (error) { bot.reply(message, 'Oops! Something went wrong.'); console.log(error); }
+                    else {
+                      var bitbucketUrl = "http://" + JIRA_CREDS + "@bitbucket.liatr.io/rest/api/1.0/"
+                      request({url: bitbucketUrl + `projects/${is.fields.project.key}/repos`}, function(err, res, bod) {
+                        var actions = [];
+                        var callbacks = [];
+                        var repos = JSON.parse(bod);
 
-    //var JIRA_CREDS = process.env.JIRA_API_CREDENTIALS;
-    //var jira = new JiraClient( {
-    //  host: process.env.JIRA_HOST,
-    //  protocol: "http",
-    //  basic_auth: {
-    //    username: JIRA_CREDS.split(":")[0],
-    //    password: JIRA_CREDS.split(":")[1]
-    //  }
-    //});
-    //const auth = "Bearer " + process.env.SLACK_ACCESS_TOKEN;
-    //const key = message.match[1];
-    //const options = { 
-    //  method: 'GET', url: 'https://liatrio.slack.com/api/users.list',
-    //  headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': auth }
-    //};
-    //request(options, function(error, response, body) {
-    //  if (!error && response.statusCode == 200) {
-    //    const list = JSON.parse(body);
-    //    for (var i = 0; i < list.members.length; i++){
-    //      if (list.members[i].id == message.user){
-    //        jira.issue.assignIssue({ issueKey: key, assignee: list.members[i].profile.email.split("@")[0]}, function(error, issue) {
-    //          if (error) { bot.reply(message, 'failure'); console.log(error); }
-    //          else {
-    //            jira.issue.getIssue({ issueKey: key}, function(error, is) {
-    //              if (error) { console.log(error); }
-    //              else {
-    //                var bitbucketUrl = "http://" + JIRA_CREDS + "@bitbucket.liatr.io/rest/api/1.0/"
-    //                request({url: bitbucketUrl + `projects/${is.fields.project.key}/repos`}, function(err, res, bod) {
-    //                  var actions = [];
-    //                  var callbacks = [];
-    //                  var repos = JSON.parse(bod);
+                        for (var i = 0; i < repos.values.length; i++){
+                          var action = { type: "button", name: `${repos.values[i].slug}`, text: `${repos.values[i].slug}`, value: `${repos.values[i].slug}` }
+                          var callback = { 
+                            pattern: `${repos.values[i].slug}`, 
+                            callback: function(reply, convo) { 
+                              bitbucketUrl = "http://" + process.env.JIRA_API_CREDENTIALS + "@bitbucket.liatr.io/rest/branch-utils/1.0/"
+                              var branch = key + "-" + is.fields.description.replace(/ /g, "-");
+                              var postData = {
+                                name: branch,
+                                startPoint: "refs/heads/master"
+                              }
+                              request({url: bitbucketUrl + "projects/" +  key.split("-")[0] + "/repos/" + reply.text + "/branches", 
+                                       headers: { 'X-Atlassian-Token': 'no-check' }, json: true,
+                                       method: 'post', body: postData}, function(err, res, bod) {
+                                if (error) { console.log(err); }
+                                else { 
+                                  var link = "http://bitbucket.liatr.io/projects/" + key.split("-")[0] + "/repos/" + "pipeline-demo-application" + "/browse"
+                                  convo.say(`branch has been made on the repo <${link}|${reply.text}>` + " with the branch name `" + branch + "`"); 
+                                  convo.next(); 
+                                }
+                              });
+                            } 
+                          }
 
-    //                  for (var i = 0; i < repos.values.length; i++){
-    //                    var action = { type: "button", name: `${repos.values[i].slug}`, text: `${repos.values[i].slug}`, value: `${repos.values[i].slug}` }
-    //                    var callback = { 
-    //                      pattern: `${repos.values[i].slug}`, 
-    //                      callback: function(reply, convo) { 
-    //                        console.log(reply); 
-    //                        convo.say('branch has been made on the repo ' + repos.values[i].slug); 
-    //                        convo.next(); 
-    //                      } 
-    //                    }
+                          actions.push(action);
+                          callbacks.push(callback);
+                        }
+                        var callback = { 
+                          default: true, 
+                          callback: function(reply, convo) { 
+                            console.log(reply); 
+                            convo.say('cool stuff!');
+                          } 
+                        }
+                        callbacks.push(callback);
 
-    //                    actions.push(action);
-    //                    callbacks.push(callback);
-    //                  }
-    //                  var callback = { 
-    //                    default: true, 
-    //                    callback: function(reply, convo) { 
-    //                      console.log(reply); 
-    //                      convo.say('cool stuff!');
-    //                    } 
-    //                  }
-    //                  callbacks.push(callback);
-
-    //                  let msg = {
-    //                    text: "Issue has been assigned to you. Want to create a branch?",
-    //                    attachments: [{
-    //                      fallback: 'actions',
-    //                      callback_id: "take_ticket_callback",
-    //                      actions: actions
-    //                    }]
-    //                  };
-    //                  bot.startConversation(message, function(err, convo) {
-    //                    convo.ask(msg, callbacks);
-    //                  });
-    //                });
-    //              }
-    //            });
-    //          }
-    //        });
-    //      }
-    //    }
-    //  }
-    //  else {
-    //    console.log(error);
-    //  }
-    //});
+                        let msg = {
+                          text: "Issue has been assigned to you. Want to create a branch?",
+                          attachments: [{
+                            fallback: 'actions',
+                            callback_id: "take_ticket_callback",
+                            actions: actions
+                          }]
+                        };
+                        bot.startConversation(message, function(err, convo) {
+                          convo.ask(msg, callbacks);
+                        });
+                      });
+                    }
+                  });
+                }
+              });
+            }
+          }
+        }
+        else {
+          console.log(error);
+        }
+      });
+    }
   });
 
 
