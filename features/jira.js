8ea2911ccf6323dd2dfd449152d5e8cd15192655
Steven O <steveno@liatrio.com>
2019-01-24 11:38:45 -0800

    debugging

--- features/jira.js
+++ features/jira.js
@@ -116,6 +116,8 @@ function jira(bot, controller) {
                 console.log(`** Response from selectBoard: ${JSON.stringify(response, null, 2)}`);
                 return getTicketsForBoard(bot, message, response, status);
             }, rejection => {
+                console.log(!rejection);
+                console.log(!rejection.callback_id);
                 if (!rejection || !rejection.callback_id) {
                     console.log(`Rejection: ${JSON.stringify(rejection, null, 2)}`);
                 }
