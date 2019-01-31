9203c22760121bf705ef838e05f8adf48998dcae
Justin Bankes <jbankes1@gmail.com>
2019-01-30 23:57:13 -0500

    LIB-102 initial user addition feature

--- features/user.js
+++ features/user.js
@@ -4,51 +4,63 @@
 
 let ERROR_NEED_TAG='Cool! New member. Let\'s make sure to tag the user with the `@`.'
 
-// verify added user is tagged, return cleaned up string
-function check_for_tagged_name (dirty_id) {
+// verify added user is tagged, return cleaned up string or false if id isn't correct
+function check_and_clean_id (dirty_id) {
   if (dirty_id.startsWith('<@')) {
     dirty_id = dirty_id.replace('<@','');
     dirty_id = dirty_id.replace('>','');
     console.log('new ID is: ' + dirty_id );
     return dirty_id;
   } else {
-    return 0;
+    return false;
   }
 }
 
-function get_slack_id() {
-
+// verify the user exists in the database, returns user if found
+function check_for_user(controller, user_id) {
+  return user = controller.storage.users.get(user_id, function(err, user) {});
 }
 
-function check_for_user() {
-
+// add user to database
+function create_user_in_bot_db(bot, controller, new_user) {
+  
+  //get name
+  bot.api.users.info({user: new_user}, function(err, response) {
+    if (!response.user) {
+      bot.replyInThread(message, 'That\'s not a Slack user.');
+    } else {
+      var name = response.user.profile.display_name;
+      console.log('Username will be set as ' + name );
+      user = {
+        id: new_user,
+        name: name,
+        beerjar: 0
+      };
+      controller.storage.users.save(user, function(err, id) {});
+    }
+  });
 }
 
-function add_new_user() {
-
-}
-
-function remove_user() {
-
-}
-
+// main user feature function
 function user(bot, controller) {
   controller.hears(['add user (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
     
     var dirty_id = message.match[1];
-    if(!check_for_tagged_name(dirty_id)) {
+    var clean_id = check_and_clean_id(dirty_id);
+    if(!clean_id) {
       bot.replyInThread(message, ERROR_NEED_TAG);
       return;
-    }
-
-    var new_user = get_slack_id(dirty_id);
-
-    if(!check_for_user()) {
-      add_new_user();
-    } else {
-
-    }
-    bot.replyInThread(message, 'This is successful');
+    } 
+    
+    let waiting_for_check = check_for_user(controller, clean_id);
+    waiting_for_check.then(function(result) { 
+      if(result) {
+        bot.replyInThread(message, 'User already exists, have fun! :shaka:');
+      } else {
+        create_user_in_bot_db(bot, controller, clean_id);
+        bot.reply(message, 'Congratulations and Welcome! :parrot:');
+      }
+    })
   });
 }
 
